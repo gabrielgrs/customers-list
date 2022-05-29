1ff5b59a621348cb0eb7412053bc0bf2a37f4543
Gabriel R <grxgabriel@gmail.com>
2022-05-29 13:07:24 -0300

    :stethoscope: Add @/ to absolute import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
+import { Fragment } from 'react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { Fragment } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
