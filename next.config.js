91520daebd45c524bf2e8f8f443f794ffe470223
Gabriel R <grxgabriel@gmail.com>
2022-05-30 00:15:37 -0300

    :technologist: Add sentry configurations

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
+const { withSentryConfig } = require('@sentry/nextjs')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const moduleExports = {
   reactStrictMode: true,
   env: {
     API_URL: process.env.API_URL,
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_PUBLIC_ANON_KEY: process.env.SUPABASE_PUBLIC_ANON_KEY,
+    SENTRY_DSN: process.env.SENTRY_DSN,
   },
 }
 
-module.exports = nextConfig
+const sentryWebpackPluginOptions = {
+  silent: true,
+}
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
